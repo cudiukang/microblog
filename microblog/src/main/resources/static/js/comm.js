2c405333435182a06460093bd55b59cc97a19a8c
cuidukang <56304567@qq.com>
2020-01-05 13:45:33 +0800

    优化登录功能

--- microblog/src/main/resources/static/js/comm.js
+++ microblog/src/main/resources/static/js/comm.js
@@ -338,6 +338,58 @@ function searchOnCurrent(){
 	initTable();
 }
 
+//打开登录模态框
+function login(){
+	$("#userModal").modal("show");
+}
+
+//检查是否登录
+function checkLogin(){
+	var param = $("#userForm").serialize();
+	$.ajax({
+		url:"/login",
+		type:"post",
+		data:param,
+		dataType:"json",
+		success:function(res){
+			if(res.state){
+				alertModel("登录成功。");
+			}else{
+				alertModel("用户名或者密码错误。");
+			}
+			$("#userName").val("");
+			$("#userPwd").val("");
+			//隐藏模态框
+			$("#userModal").modal("hide");
+			//隐藏登录按钮,显示操作按钮
+			$("#operationUl,#loginUl").toggle();
+			//设置名称
+			$("#userLogo").text("您好："+res.obj.uName);
+		},
+		error:function(){
+			alertModel("系统繁忙，请稍后再试。");
+		}
+	});
+}
+
+//退出
+function quitBtn(){
+	$.ajax({
+		url:"/quit",
+		type:"post",
+		data:{},
+		dataType:"json",
+		success:function(res){
+			if(res.state){
+				location.reload();
+			}
+		},
+		error:function(){
+			alertModel("系统繁忙，请稍后再试。");
+		}
+	});
+}
+
 
 
 
--- microblog/src/main/resources/static/js/index.js
+++ microblog/src/main/resources/static/js/index.js
@@ -6,59 +6,6 @@ $(function(){
 	initHotTitle();
 });
 
-
-
-function login(){
-	$("#userModal").modal("show");
-}
-
-function checkLogin(){
-	var param = $("#userForm").serialize();
-	$.ajax({
-		url:"/login",
-		type:"post",
-		data:param,
-		dataType:"json",
-		success:function(res){
-			if(res.state){
-				alertModel("登录成功。");
-			}else{
-				alertModel("用户名或者密码错误。");
-			}
-			$("#userName").val("");
-			$("#userPwd").val("");
-			//隐藏模态框
-			$("#userModal").modal("hide");
-			//隐藏登录按钮,显示操作按钮
-			$("#operationUl,#loginUl").toggle();
-			//设置名称
-			$("#userLogo").text("您好："+res.obj.uName);
-		},
-		error:function(){
-			alertModel("系统繁忙，请稍后再试。");
-		}
-	});
-}
-
-//退出
-function quitBtn(){
-	$.ajax({
-		url:"/quit",
-		type:"post",
-		data:{},
-		dataType:"json",
-		success:function(res){
-			if(res.state){
-				location.reload();
-			}
-		},
-		error:function(){
-			alertModel("系统繁忙，请稍后再试。");
-		}
-	});
-}
-
-
 //表格初始化
 function initTable(){
 	// 先销毁表格
